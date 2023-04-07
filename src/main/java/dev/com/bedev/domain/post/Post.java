e516e72ba2346184a0245c2f5fe55591506c7b70
Seo, Myoungjin <sih5673@gmail.com>
2023-04-08 01:21:56 +0900

    feat: Post Entity

---
+++ src/main/java/dev/com/bedev/domain/post/Post.java
@@ -0,0 +1,51 @@
+package dev.com.bedev.domain.post;
+
+import dev.com.bedev.domain.date.BaseTimeEntity;
+import dev.com.bedev.domain.category.Category;
+import dev.com.bedev.domain.project.Project;
+import dev.com.bedev.domain.tag.Tag;
+import dev.com.bedev.domain.user.User;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
+@Getter
+@Entity
+public class Post extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "project_id")
+    private Project project;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @OneToMany(mappedBy = "posts", cascade = CascadeType.REMOVE)
+    private List<Tag> tag = new ArrayList<>();
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String content;
+
+    @Enumerated(EnumType.STRING)
+    private Category category;
+
+    @Column(nullable = false)
+    private String views;
+
+    @Column(nullable = false)
+    private String stack;
+}
