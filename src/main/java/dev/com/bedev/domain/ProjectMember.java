6742f936d63100117da0359142b71dc313f02ca3
Seo, Myoungjin <sih5673@gmail.com>
2023-04-08 01:25:23 +0900

    feat: Entity

---
+++ src/main/java/dev/com/bedev/domain/ProjectMember.java
@@ -0,0 +1,27 @@
+package dev.com.bedev.domain;
+
+import dev.com.bedev.domain.project.Project;
+import dev.com.bedev.domain.user.User;
+import jakarta.persistence.*;
+import lombok.*;
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
+@Getter
+@Entity
+public class ProjectMember {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id",nullable = false)
+    User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "project_id",nullable = false)
+    Project project;
+}
