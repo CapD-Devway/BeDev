71672d05b73c55188f2f3648060929ab09b3376f
Seo, Myoungjin <sih5673@gmail.com>
2023-04-08 01:25:07 +0900

    feat: User Entity

---
+++ src/main/java/dev/com/bedev/domain/user/User.java
@@ -0,0 +1,36 @@
+package dev.com.bedev.domain.user;
+
+import dev.com.bedev.domain.date.BaseTimeEntity;
+import dev.com.bedev.domain.post.Post;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
+public class User extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false)
+    private Long id;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String name;
+
+    @OneToMany(mappedBy = "project", cascade = CascadeType.REMOVE)
+    private List<Post> posts = new ArrayList<>();
+
+
+}
