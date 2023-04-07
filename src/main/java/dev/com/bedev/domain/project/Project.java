cd03e15ec04020bb323dd0a1e1e1df4fade82c2c
Seo, Myoungjin <sih5673@gmail.com>
2023-04-08 01:22:04 +0900

    feat: Project Entity

---
+++ src/main/java/dev/com/bedev/domain/project/Project.java
@@ -0,0 +1,35 @@
+package dev.com.bedev.domain.project;
+
+import dev.com.bedev.domain.date.BaseTimeEntity;
+import dev.com.bedev.domain.post.Post;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
+@Getter
+@Entity
+public class Project extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column
+    private String content;
+
+    @OneToMany(mappedBy = "project", cascade = CascadeType.REMOVE)
+    private List<Post> posts = new ArrayList<>();
+
+
+
+}
