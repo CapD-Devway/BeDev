bde62ccd52977fb0714468737137201cde9f7be1
Seo, Myoungjin <sih5673@gmail.com>
2023-04-08 01:25:43 +0900

    feat: Repository

---
+++ src/main/java/dev/com/bedev/domain/tag/TagRepository.java
@@ -0,0 +1,6 @@
+package dev.com.bedev.domain.tag;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TagRepository extends JpaRepository<Tag,Long> {
+}
---
+++ src/main/java/dev/com/bedev/domain/user/UserRepository.java
@@ -0,0 +1,6 @@
+package dev.com.bedev.domain.user;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User,Long> {
+}
