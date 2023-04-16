d88e14baecf4aab48510ac475aab391d70b84392
MyoungJinSeo <sih5673@gmail.com>
2023-04-16 23:48:29 +0900

    refactor : add createProject #3

--- src/main/java/dev/com/bedev/api/project/controller/ProjectController.java
+++ src/main/java/dev/com/bedev/api/project/controller/ProjectController.java
@@ -1,9 +1,14 @@
 package dev.com.bedev.api.project.controller;
 
+import dev.com.bedev.api.project.dto.request.ProjectRequestDto;
+import dev.com.bedev.api.project.dto.response.ProjectResponseDto;
+import dev.com.bedev.api.project.service.ProjectService;
+import dev.com.bedev.domain.project.Project;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -11,9 +16,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping
 public class ProjectController {
 
+    private final ProjectService projectService;
     @PostMapping("api/project")
-    public ResponseEntity<?> createProject(){
-
-        return ResponseEntity.ok().body("1");
+    public ResponseEntity<?> createProject(@RequestBody ProjectRequestDto projectRequestDto){
+        Project project = projectService.createProject(projectRequestDto);
+        ProjectResponseDto projectResponseDto = ProjectResponseDto.builder()
+                .id(project.getId())
+                .teamContent(project.getContent())
+                .teamName(project.getName())
+                .build();
+        return ResponseEntity.ok().body(projectResponseDto);
     }
 }
