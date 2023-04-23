e6645006f4f26bed4d14609ee7ca2cc8253b489c
Jaewan, 再完 <wodhkszxc@gmail.com>
2023-04-23 14:18:54 +0900

    refactor : 카테고리에 따른 분류 -> cancel #2

--- src/main/java/dev/com/bedev/domain/post/controller/PostController.java
+++ src/main/java/dev/com/bedev/domain/post/controller/PostController.java
@@ -4,38 +4,55 @@ import dev.com.bedev.domain.post.dto.PostRequestDto;
 import dev.com.bedev.domain.post.dto.PostResponseDto;
 import dev.com.bedev.domain.post.service.PostService;
 import lombok.RequiredArgsConstructor;
-import org.apache.coyote.Response;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
+@RequestMapping("/post")
 @RestController
 @RequiredArgsConstructor
 public class PostController {
 
     private final PostService postService;
 
+    // 팀원 모집 글 - 피드형식
+    @GetMapping
+    public ResponseEntity<List<PostResponseDto>> findAll() {
+        List<PostResponseDto> responseDtos = postService.findAll();
+        return ResponseEntity.ok(responseDtos);
+    }
+
+    /*메인 - 카테고리에 따른 분류 - cancel
+    @GetMapping("/category/{category}")
+    public ResponseEntity<List<PostResponseDto>> findByCategory(@PathVariable Category category) {
+        List<PostResponseDto> responseDtos = postService.findByCategory(category);
+        return ResponseEntity.ok(responseDtos);
+    }
+
+     */
+
     // Create
-    @PostMapping("/post")
+    @PostMapping("/write")
     public ResponseEntity save(@RequestBody PostRequestDto postRequestDto) {
         return ResponseEntity.ok(postService.save(postRequestDto));
     }
 
     // Read
-    @GetMapping("/post/{id}")
+    @GetMapping("/{id}")
     public ResponseEntity read(@PathVariable Long id) {
         return ResponseEntity.ok(postService.findById(id));
     }
 
     // Update
-    @PutMapping("/post/{postId}")
+    @PutMapping("/{postId}")
     public ResponseEntity update(@PathVariable Long id, @RequestBody PostRequestDto postRequestDto) {
         postService.update(id, postRequestDto);
         return ResponseEntity.ok(id);
     }
 
     // Delete
-    @DeleteMapping("/post/{id}")
+    @DeleteMapping("/{id}")
     public ResponseEntity delete(@PathVariable Long id) {
         postService.delete(id);
         return ResponseEntity.ok(id);
