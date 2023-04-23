65d2f76f5a84f2bc7f66735bb73f18d4172265f0
Jaewan, 再完 <wodhkszxc@gmail.com>
2023-04-23 15:09:45 +0900

    refactor : add postlist, save method adding user, project-> postEntity #2

--- src/main/java/dev/com/bedev/domain/post/service/PostService.java
+++ src/main/java/dev/com/bedev/domain/post/service/PostService.java
@@ -4,23 +4,44 @@ import dev.com.bedev.domain.post.Post;
 import dev.com.bedev.domain.post.dto.PostRequestDto;
 import dev.com.bedev.domain.post.dto.PostResponseDto;
 import dev.com.bedev.domain.post.repository.PostRepository;
+import dev.com.bedev.domain.project.Project;
+import dev.com.bedev.domain.project.ProjectRepository;
 import dev.com.bedev.domain.user.User;
 import dev.com.bedev.domain.user.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
+/*
+카테고리에 따른 게시판 분류 -> 팀원 모집 글
+ */
 @Service
 @RequiredArgsConstructor
 public class PostService {
 
-    private PostRepository postRepository;
-    private UserRepository userRepository;
+    private final PostRepository postRepository;
+    private final UserRepository userRepository;
+    private final ProjectRepository projectRepository;
 
-    //Create 로그인 시 글작성은 이후 리팩토링
+
+    public List<PostResponseDto> findAll() {
+        List<Post> posts = postRepository.findAll();
+        return posts.stream().map(PostResponseDto::new).collect(Collectors.toList());
+    }
+
+    //Create
     @Transactional
     public Long save(PostRequestDto postRequestDto) {
+        User user = userRepository.findById(postRequestDto.getUserId()).orElseThrow(() -> new IllegalArgumentException("해당하는 유저는 없습니다."));
+        Project project = projectRepository.findById(postRequestDto.getProjectId()).orElseThrow(()-> new IllegalArgumentException("해당 프로젝트는 없습니다."));
+
         Post post = postRequestDto.toEntity();
+        post.associateUser(user);
+        post.associateProject(project);
+
         postRepository.save(post);
         return post.getId();
     }
@@ -49,20 +70,4 @@ public class PostService {
 
         postRepository.delete(post);
     }
-    /* 조회수
-
-    @Transactional
-    public int viewCount(Long id) {
-
-    }
-
-     */
-
-    /*
-    메인페이지에 해당하는 list
-     */
-
-    /*
-    메인페이지에 해당하는 검색기능
-     */
 }
