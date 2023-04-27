07e999bd7f4812c365483f7f9d64cb187f8653d7
MyoungJinSeo <sih5673@gmail.com>
2023-04-27 14:27:54 +0900

    refactor : add querydsl

--- src/main/java/dev/com/bedev/domain/project/ProjectRepository.java
+++ src/main/java/dev/com/bedev/domain/project/ProjectRepository.java
@@ -2,5 +2,5 @@ package dev.com.bedev.domain.project;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ProjectRepository extends JpaRepository<Project,Long> {
+public interface ProjectRepository extends JpaRepository<Project,Long> , ProjectRepositoryCustom {
 }
