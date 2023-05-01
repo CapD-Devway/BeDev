74bc5273538c4c5be5bbcde22acb4ee0c0ed84ab
Jaewan, 再完 <wodhkszxc@gmail.com>
2023-05-02 03:51:27 +0900

    feat : add findALl, findById #3

--- src/main/java/dev/com/bedev/api/project/service/ProjectService.java
+++ src/main/java/dev/com/bedev/api/project/service/ProjectService.java
@@ -4,27 +4,43 @@ import dev.com.bedev.api.project.dto.request.ProjectRequestDto;
 import dev.com.bedev.api.project.dto.response.ProjectResponseDto;
 import dev.com.bedev.domain.project.Project;
 import dev.com.bedev.domain.project.ProjectRepository;
+import dev.com.bedev.domain.user.User;
+import dev.com.bedev.domain.user.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-@Service
-@Transactional(readOnly = true)
+import java.util.List;
+import java.util.stream.Collectors;
+
 @RequiredArgsConstructor
+@Service
 public class ProjectService {
 
+    private final UserRepository userRepository;
     private final ProjectRepository projectRepository;
 
     @Transactional
-    public Project createProject(ProjectRequestDto projectRequestDto){
+    public Long createProject(ProjectRequestDto projectRequestDto) {
 
-        Project project = Project.builder()
-                .name(projectRequestDto.getTeamName())
-                .content(projectRequestDto.getTeamContent())
-                .build();
+        User user;
+        Project project = projectRequestDto.toEntity();
+
         projectRepository.save(project);
-        return project;
+        return project.getId();
     }
 
+    @Transactional(readOnly = true)
+    public List<ProjectResponseDto> findAllProjects() {
+        return projectRepository.findAll().stream()
+                .map(ProjectResponseDto::new)
+                .collect(Collectors.toList());
+    }
 
+    @Transactional(readOnly = true)
+    public ProjectResponseDto findById(Long id) {
+        Project project = projectRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 프로젝트가 없습니다."));
+        return new ProjectResponseDto(project);
+    }
 }
