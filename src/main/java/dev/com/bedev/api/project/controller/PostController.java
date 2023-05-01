1ef71ddd3a638ef1fd90c96db64642b44cd5a486
Jaewan, 再完 <wodhkszxc@gmail.com>
2023-05-02 04:07:26 +0900

    feat : add getSearchedPosts to PostController #2

--- src/main/java/dev/com/bedev/api/project/controller/PostController.java
+++ src/main/java/dev/com/bedev/api/project/controller/PostController.java
@@ -1,9 +1,14 @@
 package dev.com.bedev.api.project.controller;
 
+import dev.com.bedev.api.condition.PostSearchCondition;
 import dev.com.bedev.api.project.dto.request.PostRequestDto;
 import dev.com.bedev.api.project.dto.response.PostResponseDto;
 import dev.com.bedev.api.project.service.PostService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -18,9 +23,8 @@ public class PostController {
 
     // 팀원 모집 글 - 피드형식
     @GetMapping
-    public ResponseEntity<List<PostResponseDto>> findAll() {
-        List<PostResponseDto> responseDtos = postService.findAll();
-        return ResponseEntity.ok(responseDtos);
+    public Page<PostResponseDto> getAllPosts(Pageable pageable) {
+        return postService.findAll(pageable);
     }
 
     /*메인 - 카테고리에 따른 분류 - cancel
@@ -45,7 +49,7 @@ public class PostController {
     }
 
     // Update
-    @PutMapping("/{postId}")
+    @PutMapping("/{id}")
     public ResponseEntity update(@PathVariable Long id, @RequestBody PostRequestDto postRequestDto) {
         postService.update(id, postRequestDto);
         return ResponseEntity.ok(id);
@@ -56,5 +60,14 @@ public class PostController {
     public ResponseEntity delete(@PathVariable Long id) {
         postService.delete(id);
         return ResponseEntity.ok(id);
+    }
+
+    // 검색 결과
+    @GetMapping
+    public ResponseEntity<Page<PostResponseDto>> getSearchedPosts(
+            @PageableDefault(size = 20, sort = "id", direction = Sort.Direction.DESC) Pageable pageable,
+            @ModelAttribute PostSearchCondition searchCondition) {
+        Page<PostResponseDto> postList = postService.getSearchPosts(searchCondition, pageable);
+        return ResponseEntity.ok(postList);
     }
 }
