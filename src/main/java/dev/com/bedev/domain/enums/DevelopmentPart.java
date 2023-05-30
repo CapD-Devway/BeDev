5f3d5e373ab8a449f016909eca05d32c6abdc9eb
MyoungJinSeo <sih5673@gmail.com>
2023-05-30 23:39:21 +0900

    refactor : add davelopmentPart

--- src/main/java/dev/com/bedev/domain/enums/DevelopmentPart.java
+++ src/main/java/dev/com/bedev/domain/enums/DevelopmentPart.java
@@ -4,6 +4,6 @@ import lombok.Getter;
 
 @Getter
 public enum DevelopmentPart {
-    BACKEND, FRONTEND,IOS,ANDROID,WEB_DESIGNER
+    BackEnd_개발자, FrontEnd_개발자 ,IOS,ANDROID,WEB_Publisher,웹개발자,안드로이드_개발자,웹디자이너,UI개발자
 
 }
