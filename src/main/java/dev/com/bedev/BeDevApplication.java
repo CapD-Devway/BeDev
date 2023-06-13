b389b49fec897654db3e3019f760e90a7a570ba9
MyoungJinSeo <sih5673@gmail.com>
2023-06-14 01:22:54 +0900

    fix : BeanCreationException of firestoreOptions

--- src/main/java/dev/com/bedev/BeDevApplication.java
+++ src/main/java/dev/com/bedev/BeDevApplication.java
@@ -1,11 +1,10 @@
 package dev.com.bedev;
 
+import com.google.cloud.spring.autoconfigure.firestore.GcpFirestoreAutoConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@SpringBootApplication
-@EnableJpaAuditing
+@SpringBootApplication(exclude = {GcpFirestoreAutoConfiguration.class})
 public class BeDevApplication {
 
     public static void main(String[] args) {
